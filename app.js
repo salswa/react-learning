dfa0ef13eab274758b95194724132897c6f651f5
salswa <swatindp@gmail.com>
2023-04-30 01:28:04 +0530

    Episode-03: Laying the foundation -JSX,BABEL,PARCEL,COMPONENT

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const head = React.createElement("div", {id:"heading", className:"heading"}, 
-             [
-                React.createElement("h1", {id:"child"}, "Hello world react here!"),
-                React.createElement("h1", {id:"child"}, "Hello world react here!")
-             ]
-            );
-console.log("heading",head);
-const reactroot = ReactDOM.createRoot(document.getElementById("react-root"));
-console.log("reactroot",reactroot);
-reactroot.render(head);
+import ReactDOM from "react-dom/client";
+
+ const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+ console.log("heading", heading);
+ const elem = (
+   <div className="span">Span element</div>
+ );
+ const Title = () => {
+   return (
+      <h1 id="heading" className="title-jsx" tabIndex="1">
+         {elem}
+         Namaste React from JSX!
+      </h1>
+   )};
+ //console.log("JSXheading",JSXheading);
+
+ const newTitle = (
+   <div>
+      <Title/>
+      <h1 className="personal">Swati</h1>
+   </div>
+   
+ );
+ const HeadingComponent = () => {
+   return (
+      <>
+         COMPONENT COMPOSITION STARTS HERE!
+         <Title></Title>
+      <h1 className="heading" id="heading">
+         Namaste React from React Functional Component!
+      </h1>
+      {newTitle}
+      <>
+      <div>12345</div>
+      </>
+      </>
+      
+   )
+ };
+
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ const jsxRoot = ReactDOM.createRoot(document.getElementById('jsx-root'));
+ //jsxRoot.render(title);
+ root.render(<HeadingComponent/>);
