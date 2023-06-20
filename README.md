d8be4033c81a4eeeb239fd9961235b598e64495c
salswa <swatindp@gmail.com>
2023-06-21 00:30:07 +0530

    Episode-05: stable main branch

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 ## React Learning
+
+# Parcel
+    - Dev Build
+    - Local Server
+    - Hot Module Replacement (HMR) -> Automatic Refreshing
+    - File Watching -> written in C++
+    - Caching -> provides faster builds due to caching feature
+    - Image Optimization
+    - Minification 
+    - Bundling
+    - Compressing
+    - Content Hashing
+    - Code Splitting
+    - Differential Bundling (to support legacy browsers creates different bundles)
+    - Error Handling
+    - HTTPs -> secured hosting
+    - Tree Shaking -> applies this algorithm to remove unused code
+    - Different dev and prod bundles -> creates even more optimized bundle for prod than that of dev
+    - 
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,37 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
-let listOfRestaurants = resList;
+//let listOfRestaurants = resList;
 
 const Body = () => {
+    [listOfRestaurants, setListOfRestaurants] = useState(resList);
     return (
        <div className="body-container">
           <div className="Search">
              <input type="text" placeholder="Enter Restaurant Name"></input>
              <button>Search</button>
-             <button className= "top-filter-button"
+             <button className= "top-filter-btn"
                 onClick={ () => {
                     const filteredList = listOfRestaurants.filter(
-                        (res) => res.data.avgRating>=4 
+                        (res) => res.data.avgRating >=4 
                     );
+                    setListOfRestaurants(filteredList);
                     console.log("filteredList",filteredList);
+                    console.log("filteredList123",listOfRestaurants);
                     }
                 }
              >
                 Top Rated Restaurants
+            </button>
+            <button className="reset-filter-btn"
+                onClick={
+                    () => {
+                        setListOfRestaurants(resList);
+                    }
+                }
+            >
+                Reset Restaurant List
             </button>
           </div>
 
@@ -28,10 +41,7 @@ const Body = () => {
                    return <RestaurantCard key = { restaurant.data.id} 
                    resData= {restaurant} />
                 })
-             } 
-            
-          
-             
+             }              
           </div>
        </div>
     );
